af55c7e090215487cef00b59bd6b530bbaa7559b
NumanDemirhan <numan.dmrhn6709@gmail.com>
2026-05-13 16:40:28 +0300

    Initial Commit

---
+++ LAB-7/Lab 7.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct Sensor {
+    int sensor_id;
+    float sicaklik;
+};
+
+int main()
+{
+    long long int id;
+    int N, E;
+    float exSicaklik;
+    Sensor s1;
+    cout << "Please enter your student id: " << endl;
+    cin >> id;
+    N = id % 50;
+    E = id % 100;
+
+    fstream file;
+    file.open("sensors.bin", ios::in | ios::out | ios::binary);
+    
+    if (!file.is_open()) {
+        cout << "File couldn't open.";
+        return 1;
+    }
+
+    file.seekg(N * sizeof(Sensor), ios::beg);
+    file.read((char*)&s1, sizeof(Sensor));
+    exSicaklik = s1.sicaklik;
+    s1.sicaklik += E;
+    file.seekp(N * sizeof(Sensor), ios::beg);
+    file.write((char*)&s1, sizeof(Sensor));
+    file.close();
+
+    fstream SText;
+    SText.open(to_string(id) + "_Quiz5.txt", ios::out);
+    SText << "Number: " << id << endl;
+    SText << "Sensor Index Used in the Process: " << N << endl;
+    SText << "First Temprature: " << exSicaklik << " -> New Temprature: " << s1.sicaklik << endl;
+    SText.close();
+    return 0;
+}
+
